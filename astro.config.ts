8ce037dc797d1f3504ab6067e4b2d05c2dc63ac2
SebSept <sebastienmonterisi@yahoo.fr>
2024-11-28 22:05:19 +0100

    lint

--- astro.config.ts
+++ astro.config.ts
@@ -11,9 +11,14 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react(), sitemap(), mdx()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    sitemap(),
+    mdx(),
+  ],
   markdown: {
     remarkPlugins: [
       remarkToc,
