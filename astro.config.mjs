5affae81ed70d4fc6ff33c1df87878572414f03a
Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
2024-12-04 18:14:19 +0100

    Suite rédaction du post titre pro 2
    en mdx avec le callout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,12 +12,12 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   site: SITE.website,
   integrations: [
+    mdx(),
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
-    sitemap(),
-    mdx(),
+    sitemap()
   ],
   markdown: {
     remarkPlugins: [
